47a6b41faf58ecd119c15d7a50e202062dc996fc
Mr_FabiozZz <mr.fabiozzz@gmail.com>
2023-11-26 13:28:15 +0300

    FEAT: Added templates

---
+++ src/mock/signingData.js
@@ -0,0 +1,80 @@
+export const pageData = {
+  '/index.html': {
+    title: 'Навигация'
+  },
+  '/src/pages/login/index.html': {
+    title: 'Авторизация',
+    inputs: {
+      login: {
+        placeholder: 'Логин',
+        type: 'text'
+      },
+      password: {
+        placeholder: 'Пароль',
+        type: 'password'
+      }
+    }
+  },
+  '/src/pages/signIn/index.html': {
+    title: 'Регистрация',
+    inputs: {
+      first_name: {
+        placeholder: 'Имя',
+        type: 'text'
+      },
+      second_name: {
+        placeholder: 'Фамилия',
+        type: 'text'
+      },
+      login: {
+        placeholder: 'Логин',
+        type: 'text'
+      },
+      email: {
+        placeholder: 'Почта',
+        type: 'email'
+      },
+      password: {
+        placeholder: 'Пароль',
+        type: 'password'
+      },
+      phone: {
+        placeholder: 'Номер телефона',
+        type: 'phone'
+      }
+    }
+  },
+  '/src/pages/profile/index.html': {
+    title: 'Редактирование профиля',
+    inputs: {
+      first_name: {
+        placeholder: 'Имя',
+        type: 'text'
+      },
+      second_name: {
+        placeholder: 'Фамилия',
+        type: 'text'
+      },
+      login: {
+        placeholder: 'Логин',
+        type: 'text'
+      },
+      email: {
+        placeholder: 'Почта',
+        type: 'email'
+      },
+      phone: {
+        placeholder: 'Номер телефона',
+        type: 'phone'
+      },
+      oldPassword: {
+        placeholder: 'Старый пароль',
+        type: 'password'
+      },
+      newPassword: {
+        placeholder: 'Новый пароль',
+        type: 'password'
+      }
+    }
+  }
+};
